3f00afd1a71301b23d2235b86a8a86cccf734e0c
Ryo SUETSUGU <suetsugu.r@gmail.com>
2014-07-08 00:40:56 +0900

    Added: validation

--- app/models/app.rb
+++ app/models/app.rb
@@ -5,14 +5,14 @@ class App
   field :guid, type: String
   belongs_to :user
 
-  before_create :generate_guid
+  after_create :generate_guid
 
   validates :title, presence: true
-  validates :guid, presence: true
 
   def generate_guid
     uuid = SecureRandom.uuid.split('-').join
     uuid << Time.now.strftime("%Y%m%d%H%M%S%L") unless App.where(guid: uuid).first.nil?
     self.guid = uuid
+    save
   end
 end
