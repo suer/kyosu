264c817cbbb65f9f812ac2237e5ad5ff371778d1
Ryo SUETSUGU <suetsugu.r@gmail.com>
2014-07-08 00:35:32 +0900

    Added: validation

--- app/models/app.rb
+++ app/models/app.rb
@@ -2,5 +2,17 @@ class App
   include Mongoid::Document
   include Mongoid::Timestamps
   field :title, type: String
+  field :guid, type: String
   belongs_to :user
+
+  before_create :generate_guid
+
+  validates :title, presence: true
+  validates :guid, presence: true
+
+  def generate_guid
+    uuid = SecureRandom.uuid.split('-').join
+    uuid << Time.now.strftime("%Y%m%d%H%M%S%L") unless App.where(guid: uuid).first.nil?
+    self.guid = uuid
+  end
 end
