173432d0054ff08b11b190fb187a4b4a48896636
Ryo SUETSUGU <suetsugu.r@gmail.com>
2014-07-10 23:31:33 +0900

    Added: client and demo

--- public/kyosu.js
+++ public/kyosu.js
@@ -1,19 +1,23 @@
 
-function Kyosu(apikey) {
-    this.initialize(this, apikey);
+function Kyosu(app) {
+    this.initialize(app);
     return this;
 }
 
 (function(klass, document) {
-  klass.prototype.initialize = function(apikey) {
+  var currentScript = (function (e) {
+      if(e.nodeName.toLowerCase() == 'script') return e;
+      else return arguments.callee(e.lastChild)
+  })(document);
+  var host = currentScript.src.replace(/^https?:\/\/(.[^\/]*)\/.*/,'$1');
+
+  klass.prototype.initialize = function(app) {
     this.app = app;
-    this.dispatcher = new WebSocketRails("localhost:3000/websocket", true);
-    this.channels = []
+    this.dispatcher = new WebSocketRails(host + "/websocket", true);
   }
 
   klass.prototype.subscribe = function(channelName){
     var channel = this.dispatcher.subscribe(channelName)
-    this.channels[channelName] = channel
     return channel;
   };
 })(Kyosu, document);
